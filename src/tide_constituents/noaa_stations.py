a3de5429c57ffbd3c76e38ec4ed08ce1dc3c3c2c
taataam <taher.chegini@gmail.com>
2019-06-24 21:47:10 -0500

    added script for computing tide constituents based on date range and lat and lon

---
+++ src/tide_constituents/noaa_stations.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+
+def deg2float(old):
+    direction = {'N':1, 'S':-1, 'E': 1, 'W':-1}
+    new = old.replace(u'°',' ').replace('\'',' ')
+    new = new.split()
+    new_dir = new.pop()
+    return (float(new[0]) + float(new[1])/60.0) * direction[new_dir]
+
+
+def parse(url, tag):
+    import requests
+    from bs4 import BeautifulSoup
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup.find_all(tag)
+
+
+def get_stations_id():
+    tags = parse('https://tidesandcurrents.noaa.gov/stations.html?type=Water%20Levels&sort=1', 'a')
+    stations_id = []
+
+    for t in tags:
+        if t.has_attr('href'):
+            if 'waterlevels.html?id' in t['href']:
+                stations_id.append(re.findall(r'\d+', t['href'])[0])
+    return stations_id
+
+
+def get_stations_info(stations_id):
+    import re
+
+
+    established, lats, lons = [], [], []
+    if isinstance()
+    for st in stations_id:
+        tags = parse('https://tidesandcurrents.noaa.gov/stationhome.html?id=' + st, 'td')
+        for i in range(len(tags)-1):
+            t = tags[i].contents[0]
+            if isinstance(t, str):
+                t = t.replace(':', '')
+                if t == 'Established':
+                    established.append(tags[i+1].contents[0])
+                elif t == 'Latitude':
+                    lats.append(deg2float(tags[i+1].contents[0]))
+                elif t == 'Longitude':
+                    lons.append(deg2float(tags[i+1].contents[0]))
+    return established, lats, lons
+
+
+stations_id = get_stations_id()
+established, lats, lons = get_stations_info(stations_id)
+df = pd.DataFrame({'ID': stations_id, 'Established': established, 'Longitude':lons, 'Latitude':lats})
+df.to_csv('noaa_stations.csv', index=False)
---
+++ src/tide_constituents/tide_constituents.py
@@ -0,0 +1,77 @@
+import pandas as pd
+from shapely.ops import nearest_points
+from shapely.geometry import Point, MultiPoint
+from py_noaa import coops
+from tappy import tappy
+
+
+def get_tides(start, end, lon, lat):
+    """
+    date format: YYYYDDMM
+    Returns: phase, amplitude
+    """
+    stations = pd.read_csv('noaa_stations.csv', parse_dates=[1])
+    coords = [Point(ln, lt) for ln, lt in zip(stations.Longitude, stations.Latitude)]
+    nearest_station_loc = nearest_points(Point(lon, lat), MultiPoint(coords))[1]
+    idx = coords.index(nearest_station_loc)
+
+    df_water_levels = coops.get_data(
+        begin_date=start,
+        end_date=end,
+        stationid=stations.iloc[idx].ID,
+        product="water_level",
+        datum="MSL",
+        units="metric",
+        time_zone="gmt")
+    
+    dates = pd.to_datetime(df_water_levels.index) # a datetime.datetime list of dates
+    elevation = df_water_levels.water_level.values # a list of surface elevation values
+
+    # Set up the bits needed for TAPPY. This is mostly lifted from
+    # tappy.py in the baker function "analysis" (around line 1721).
+    quiet = True
+    debug = False
+    outputts = False
+    outputxml = False
+    ephemeris = False
+    rayleigh = 1.0
+    print_vau_table = False
+    missing_data = 'ignore'
+    linear_trend = False
+    remove_extreme = False
+    zero_ts = None
+    filter = None
+    pad_filters = None
+    include_inferred = True
+
+    if rayleigh:
+        ray = float(rayleigh)
+
+    x = tappy.tappy(
+        outputts = outputts,
+        outputxml = outputxml,
+        quiet=quiet,
+        debug=debug,
+        ephemeris=ephemeris,
+        rayleigh=rayleigh,
+        print_vau_table=print_vau_table,
+        missing_data=missing_data,
+        linear_trend=linear_trend,
+        remove_extreme=remove_extreme,
+        zero_ts=zero_ts,
+        filter=filter,
+        pad_filters=pad_filters,
+        include_inferred=include_inferred,
+        )
+
+    x.dates = dates
+    x.elevation = elevation
+    package = x.astronomic(x.dates)
+    (x.zeta, x.nu, x.nup, x.nupp, x.kap_p, x.ii, x.R, x.Q, x.T, x.jd, x.s, x.h, x.N, x.p, x.p1) = package
+    (x.speed_dict, x.key_list) = x.which_constituents(len(x.dates),
+            package, rayleigh_comp=ray)
+
+    # the analysis
+    x.constituents()
+    
+    return df_water_levels, x.phase, x.r
